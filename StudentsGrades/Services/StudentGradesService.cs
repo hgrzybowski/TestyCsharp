dd6d2765d9d27d06825b354f397f89ce0e4a6e46
hgrzybowski <h-grzybowski@wp.pl>
2019-05-31 13:34:03 +0200

    AddGrade

---
+++ StudentsGrades/Services/StudentGradesService.cs
@@ -0,0 +1,15 @@
+﻿using StudentsGrades.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentsGrades.Services
+{
+    public class StudentGradesService
+    {
+        public decimal Calculate(IEnumerable<Grade>grades)
+        {
+            var gradesSum = grades.Sum(g => g.Value * g.Weight);
+            return gradesSum / grades.Sum(g => g.Weight);
+        }
+    }
+}
