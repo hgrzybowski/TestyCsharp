5f37ae19f8dfe07db069fcc2e3de347d7031588a
hgrzybowski <h-grzybowski@wp.pl>
2019-06-01 09:21:13 +0200

    AddXunitTestCmd

---
+++ StudentGrades.Test.Xunit.Test.Cmd/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StudentGrades.Test.Xunit.Test.Cmd
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
